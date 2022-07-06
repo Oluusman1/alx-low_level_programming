faae99472ed025b351ce3a4c46c42cafdef8c2eb
Oluusman1 <oluwatobi.usman61@gmail.com>
2022-07-06 13:54:20 -0700

    putchar1

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -15,7 +15,7 @@ int _putchar(char c)
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- * 
+ *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
